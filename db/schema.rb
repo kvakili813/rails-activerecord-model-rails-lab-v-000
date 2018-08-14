7cf60bf24d3da49ab0737e3a3f9aa654c4b413d6
Kayvan Vakili <kvakili813@gmail.com>
2018-08-14 00:40:18 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@
 ActiveRecord::Schema.define(version: 20180814003555) do
 
   create_table "students", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
